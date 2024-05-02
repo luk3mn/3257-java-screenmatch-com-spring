578605c3f58eff5e4ae10b86d547b1f219c665d3
luk3mn <lucasrenan2015@gmail.com>
2024-05-02 16:07:03 -0300

    feat: peek, map, filter, find, container (Optional) and DoubleSummaryStatistics

---
+++ src/main/java/br/com/alura/screenmatch/annotations/Optional.java
@@ -0,0 +1,37 @@
+package br.com.alura.screenmatch.annotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Optional {
+    public static void main(String[] args) {
+//        public Optional<String> getNome() {
+//            // O nome pode ser null
+//            return Optional.ofNullable(nome);
+//        }
+
+//        Optional<String> optionalNome = getNome();
+//
+//        optionalNome.ifPresent(System.out::println); // Só irá printar o nome se não for null
+//
+//        String nome = optionalNome.orElse("Nome não disponível"); // Irá retornar "Nome não disponível" caso nome seja null
+//
+
+//        List<Integer> numeros = new ArrayList<>();
+//        for (int i = 1; i <= 100; i++) {
+//            numeros.add(i);
+//        }
+//
+//        // Utilizando parallelStream para encontrar um elemento qualquer em paralelo
+//        Optional<Integer> numeroQualquer = numeros.parallelStream()
+//                .filter(numero -> numero % 10 == 0) // Filtra os números que são múltiplos de 10
+//                .findAny();
+//
+//        if (numeroQualquer.isPresent()) {
+//            System.out.println("Encontrado: " + numeroQualquer.get());
+//        } else {
+//            System.out.println("Nenhum número encontrado.");
+//        }
+
+    }
+}
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -9,10 +9,7 @@ import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -39,9 +36,10 @@ public class Principal {
             temporadas.add(dadosTemporada);
 
         }
+//        temporadas.forEach(System.out::println);
 
-        temporadas.forEach(System.out::println);
 
+/*
 //        for(int i = 0; i < dados.totalTemporadas(); i++){
 //            List<DadosEpsodio> episodiosTemporada = temporadas.get(i).epsodios();
 //            for(int j = 0; j< episodiosTemporada.size(); j++){
@@ -56,7 +54,11 @@ public class Principal {
 
         // forma simplificada
         temporadas.forEach(System.out::println);
+*/
 
+
+
+/*
         List<DadosEpsodio> dadosEpsodios = temporadas.stream()
                 .flatMap(t -> t.epsodios().stream())
 //                .toList(); // => operação final: lista imutavel
@@ -68,6 +70,7 @@ public class Principal {
         dadosEpsodios.stream()
             .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
             .sorted(Comparator.comparing(DadosEpsodio::avaliacao).reversed())
+            .peek(e -> System.out.println("Ordenação" + e))
             .limit(5)
             .forEach(System.out::println);
 
@@ -77,6 +80,10 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         epsodios.forEach(System.out::println);
+*/
+
+
+
 
         List<Epsodio> episodios = temporadas.stream()
                 .flatMap(t -> t.epsodios().stream()
@@ -85,6 +92,21 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
+/*
+        System.out.println("Digite um trecho do título do episódio");
+        var trechoTitulo = leitura.nextLine();
+        Optional<Epsodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().contains(trechoTitulo))
+                .findFirst();
+        if(episodioBuscado.isPresent()){
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+        } else {
+            System.out.println("Episódio não encontrado!");
+        }
+*/
+
+/*
         System.out.println("A partir de que ano você deseja ver os episódios? ");
         var ano = leitura.nextInt();
         leitura.nextLine();
@@ -94,12 +116,31 @@ public class Principal {
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         episodios.stream()
-                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
-                .forEach(e -> System.out.println(
-                        "Temporada: " + e.getTemporada() +
-                                " Episódio: " + e.getTitulo() +
-                                " Data lançamento: " + e.getDataLancamento().format(formatador)
-                ));
+            .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+            .forEach(e -> System.out.println(
+                    "Temporada: " + e.getTemporada() +
+                            " Episódio: " + e.getTitulo() +
+                            " Data lançamento: " + e.getDataLancamento().format(formatador)
+            ));
+*/
+
+
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Epsodio::getTemporada,
+                        Collectors.averagingDouble(Epsodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
+
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Epsodio::getAvaliacao));
+
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor Epsodio: " + est.getMax());
+        System.out.println("Pior Epsodio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
 
 
 
